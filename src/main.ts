5f2291148bc8eab4e95776cab12d36e596b4917b
Sebastián André Ramírez Méndez <119006425+Yorusito@users.noreply.github.com>
2023-09-24 10:10:08 -0500

    Delete src directory

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-const developer : "Developer" = 'Developer';
-
-export function hello(who: string = developer): string {
-    return `Hello, ${who}`;
-}
-
-let result: string = hello();
-console.log(result);
